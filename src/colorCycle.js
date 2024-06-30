d382b0c593cc6990a35775e072e5f72d75baa52e
Kevin Mooney <kmooney@gmail.com>
2024-06-29 19:26:55 -0500

    tweak the color offsets and lightness for buttons

--- src/colorCycle.js
+++ src/colorCycle.js
@@ -1,7 +1,7 @@
 let r = document.querySelector(':root')
 let color = 0.0
 export const colorCycle = () => {
-    color = color + 0.15
+    color = color + 0.08
     r.style.setProperty('--color-family', color % 360 + 1)
     requestAnimationFrame(colorCycle)
 } 
